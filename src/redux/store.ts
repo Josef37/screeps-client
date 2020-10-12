7fb8cf2b265306c72d52ff78f3e15b6f50c214cf
Josef Wittmann <josefwittmann42@gmail.com>
2020-10-12 06:10:49 +0200

    :heavy_plus_sign: Add redux-saga

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,16 @@
 import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit'
 import rootReducer, { RootState } from './root.reducer'
+import rootSaga from './root.saga'
+import createSagaMiddleware from 'redux-saga'
+
+const sagaMiddleware = createSagaMiddleware()
 
 const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(sagaMiddleware)
 })
+
+sagaMiddleware.run(rootSaga)
 
 export type AppDispatch = typeof store.dispatch
 export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>
